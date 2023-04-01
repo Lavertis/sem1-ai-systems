0266660d293d1f757f5c04c3535f033a9e2702a8
Rafał Kuźmiczuk <29688908+Lavertis@users.noreply.github.com>
2023-04-01 20:09:37 +0200

    fix(lab_2): don't import keras from within tensorflow

--- lab_2/introduction_lab_2.py
+++ lab_2/introduction_lab_2.py
@@ -1,14 +1,11 @@
 import numpy as np
+from keras import Sequential
 from keras.datasets import mnist
-from tensorflow.python.framework.ops import disable_eager_execution
-from tensorflow.python.keras import Sequential
-from tensorflow.python.keras.layers import Conv2D, Flatten, Dense, MaxPooling2D
-from tensorflow.python.keras.losses import SparseCategoricalCrossentropy
-from tensorflow.python.keras.optimizer_v1 import Adam
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from keras.losses import SparseCategoricalCrossentropy
+from keras.optimizers import Adam
 
 WITH_POOLING = True
-
-disable_eager_execution()
 
 train, test = mnist.load_data()
 train = (train[0][:3000], train[1][:3000])
@@ -20,7 +17,7 @@ x_train = np.expand_dims(x_train, axis=-1)
 x_test = np.expand_dims(x_test, axis=-1)
 
 epochs = 10
-class_cnt = np.unique(y_train).shape[0]
+class_cnt = np.unique(y_train).shape[0]  # 10, because we have 10 digits
 filter_cnt = 32
 learning_rate = 0.0001
 act_func = 'relu'
@@ -36,7 +33,7 @@ model.add(Conv2D(
 ))
 if WITH_POOLING:
     pooling_size = (2, 2)
-    model.add(MaxPooling2D(pooling_size))
+    model.add(MaxPooling2D(pooling_size))  # max is better than average, because borders are more important
 model.add(Flatten())
 model.add(Dense(class_cnt, activation='softmax'))
 model.compile(optimizer=Adam(learning_rate), loss=SparseCategoricalCrossentropy(), metrics=['accuracy'])
